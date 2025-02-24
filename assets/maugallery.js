2fb7d7aba5d12a92d2c83164ec5f74c3afe0b28d
Theo91500 <167212980+Theo91500@users.noreply.github.com>
2025-02-24 14:34:07 +0100

    Ajout de la fonction suivant et précédent + minifier

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -153,6 +153,7 @@
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i ;
+          index = index - 1 < 0 ? imagesCollection.length - 1 : index - 1;
         }
       });
       next =
@@ -192,6 +193,7 @@
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
+          index = index + 1 >= imagesCollection.length ? 0 : index + 1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
@@ -206,13 +208,13 @@
                         <div class="modal-body">
                             ${
                               navigation
-                                ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
+                                ? '<div class="mg-prev" onclick="prevImage()" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
                             <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
-                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
+                                ? '<div class="mg-next" onclick="nextImage()" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
                                 : '<span style="display:none;" />'
                             }
                         </div>
